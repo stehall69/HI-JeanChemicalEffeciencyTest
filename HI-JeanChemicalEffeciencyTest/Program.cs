243007395511ba2ed9393314ca27ab8425b2de90
Steven Hall <stehall@chbc.school.nz>
2022-03-10 13:11:42 +1300

    Add project files.

---
+++ HI-JeanChemicalEffeciencyTest/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace HI_JeanChemicalEffeciencyTest
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
